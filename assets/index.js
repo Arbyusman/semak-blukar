45bd131867a6a32f73fe15072b2691880592a34d
Arbyusman <acingman13@gmail.com>
2024-11-23 22:16:30 +0800

    Initial commit

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+// const menuButton = document.getElementById('menuButton');
+// const closeMenu = document.getElementById('closeMenu');
+// const offCanvasMenu = document.getElementById('offCanvasMenu');
+
+// menuButton.addEventListener('click', () => {
+//     offCanvasMenu.classList.remove('hidden');
+//     offCanvasMenu.querySelector('div').classList.remove('-translate-x-full');
+// });
+
+// closeMenu.addEventListener('click', () => {
+//     offCanvasMenu.querySelector('div').classList.add('-translate-x-full');
+//     setTimeout(() => {
+//         offCanvasMenu.classList.add('hidden');
+//     }, 800); 
+// });
